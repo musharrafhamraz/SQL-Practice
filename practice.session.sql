5fa09854115ea0cb2770d795975d66eaa8194a89
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-11-18 16:00:40 +0500

    DML done.

--- practice.session.sql
+++ practice.session.sql
@@ -19,3 +19,57 @@ VALUES
 
 
 SELECT * FROM users;
+
+-- DML Commands
+
+CREATE TABLE IF NOT EXISTS employees(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    department VARCHAR(255) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL
+    );
+
+-- Inserting into the table
+
+INSERT INTO employees( name, email, department, salary)
+VALUES
+    ('Musharraf', 'musharraf02@gmail.com', 'CS', 230000.0),
+    ('ALi', 'aLif02@gmail.com', 'AI', 270000.0),
+    ('Khurram', 'khurram@gmail.com', 'CS', 220000.0),
+    ('farhan', 'farhan@gmail.com', 'AI', 560000.0),
+    ('hassan', 'hassan@gmail.com', 'CS', 260000.0),
+    ('zammer', 'zammer@gmail.com', 'ELE', 2120000.0),
+    ('maqsood', 'maqsood@gmail.com', 'AGRI', 230000.0),
+    ('main', 'main@gmail.com', 'IT', 234000.0);
+
+
+-- Reading from the table
+
+
+SELECT * FROM employees;
+
+SELECT * FROM employees WHERE department='CS';
+
+SELECT * FROM employees WHERE salary > 230000;
+
+SELECT department, salary FROM employees;
+
+
+-- Update The Table
+
+UPDATE employees SET salary = 10000 WHERE department = 'CS';
+
+SELECT * FROM employees WHERE department='CS';
+
+UPDATE employees SET name = 'Musharraf Hamraz' WHERE name = 'Musharraf';
+
+
+-- Delete From Table
+
+Delete From employees WHERE department = 'CS';
+
+SELECT * FROM employees;
+
+
+
